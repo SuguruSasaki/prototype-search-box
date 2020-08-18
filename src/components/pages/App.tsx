2e15919225aed3c2887880daff36c0e6b09c3da7
Suguru Sasaki Mizuno <sugurusasaki@SugurunoMacBook-puro.local>
2020-08-18 09:31:50 +0900

    wip: search

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Searchbox from "../atoms/Searchbox";
+import { word, results, IContents } from "../../data/words";
 
 function App() {
+  const [contents, setContents] = useState<string[]>([]);
   const handler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+    const query = e.target.value;
+
+    if (word[query]) {
+      const index = word[query];
+      if (results[index]) {
+        const output = results[index].map((data: IContents) => data.text);
+        setContents(output);
+      }
+    } else {
+      setContents([]);
+    }
   };
   return (
     <Contents className="App">
       <Title>Search Prototype</Title>
       <Searchbox changeHandler={handler} />
+      <StyledUL>
+        {contents.map((data) => {
+          return <StyledLI>{data}</StyledLI>;
+        })}
+      </StyledUL>
     </Contents>
   );
 }
@@ -24,6 +41,16 @@ const Title = styled.h1`
   font-size: 24px;
   font-weight: bolder;
   margin: 0 0 32px 0;
+`;
+
+const StyledUL = styled.ul`
+  margin: 32px 0 0 0;
+`;
+
+const StyledLI = styled.li`
+  & + li {
+    margin: 16px 0 0 0;
+  }
 `;
 
 export default App;
